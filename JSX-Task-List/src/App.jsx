b8602010e9863f032aa343be2cfe1bf65294fde2
FrancescoTullio <francescotullio1992@gmail.com>
2024-12-11 17:21:39 +0100

    feat:prima milestone.

--- JSX-Task-List/src/App.jsx
+++ JSX-Task-List/src/App.jsx
@@ -6,38 +6,45 @@ function App() {
   const uncompliteElem = [];
 
   tasks.forEach((curTask) => {
-    if(curTask.state === "completed") {
+    if (curTask.state === "completed") {
       compliteElem.push(curTask);
     } else {
       uncompliteElem.push(curTask)
     }
   })
 
-  const uncomplitedListElem = () => {
-    return uncompliteElem.map((curElem, index) => {
-      <li key={index}>
-        <p>{curElem.title}<span>{curElem.state}</span></p>
-        <p>priority: {curElem.priority}</p>
-        <p>est. time: {curElem.estimatedTime}</p>
-      </li>
-    })
-  }
+  //  console.log(compliteElem)
+  //  console.log(uncompliteElem)
 
-  const complitedListElem = () => {
-    return compliteElem.map ((curElem, index) => {
-      <li key={index}>
-        <p>{curElem.title}<span>{curElem.state}</span></p>
-        <p>priority: {curElem.priority}</p>
-        <p>est. time: {curElem.estimatedTime}</p>
-      </li>
-    })
-  }
 
+//genero la lista per i complited task
+   const complitedList = compliteElem.map((curTask, index) =>{ return <li key={index}>
+    <p>{curTask.title} <span>{curTask.state}</span></p>
+    <p>priority: {curTask.priority}</p>
+    <p>est.time: {curTask.estimatedTime}</p>
+   </li>})
+
+
+
+//genero la lista per gli uncomplited task
+  const uncomplitedList = uncompliteElem.map((curTask, index) => { return <li key={index}>
+    <p>{curTask.title} <span>{curTask.state}</span></p>
+    <p>priority: {curTask.priority}</p>
+    <p>est.time: {curTask.estimatedTime}</p>
+  </li>
+  })
+
+//qui c'è il return che torna in pagina
   return (
     <>
-      <h1>task manager</h1>
-      <p>current task ({uncompliteElem.length})</p>
-      <ul>{uncomplitedListElem()}</ul>
+    <h1>task manager</h1>
+    <p>current task ({uncomplitedList.length})</p>
+    <ul>{uncomplitedList}</ul>
+    
+
+    <hr />
+    <p>complited task ({complitedList.length})</p>
+    <ul>{complitedList}</ul>
     </>
   )
 }
